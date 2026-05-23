6e769d3c4cef0294a43a1901ae89a1bcc7acf801
Fábio Nunes <fabionascfilho@gmail.com>
2026-05-23 12:11:45 -0300

    feat: adiciona componentes CertificateCard e SettingSection e corrige caminhos de importação

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import './globals.css'
 import LayoutClient from './LayoutClient';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={cn("font-sans", geist.variable)}>
       <body className="bg-heineken-light">
         <LayoutClient>{children}</LayoutClient>
       </body>
